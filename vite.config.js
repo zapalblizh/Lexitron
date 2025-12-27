95ef76af2a6149d44c663a06327c5f87d33f8e26
zapalblizh <alexander@zapparov.dev>
2025-12-27 10:09:15 +0100

    Setting up for netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,31 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-    if (command === 'serve') {
-        return {
-            plugins: [react(), tailwindcss()],
-            server: {
-                port: 3000,
-                open: true,
-                host: true
-            }
-        }
-
-    } else {
-        // Build for production
-        return {
-            plugins: [react(), tailwindcss()],
-            build: {
-                outDir: '_site',
-                sourcemap: false,
-                minify: 'esbuild',
-                rollupOptions: {
-                    output: {
-                        manualChunks: {
-                            vendor: ['react', 'react-dom']
-                        }
-                    }
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 3000,
+        open: true,
+        host: true
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: false,
+        minify: 'esbuild',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom']
                 }
             }
         }
