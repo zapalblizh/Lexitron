f48c1b4e40257c405296ca7cbe69898bd88508fe
zapalblizh <alexander@zapparov.dev>
2025-12-27 10:21:26 +0100

    Add Netlify support by including netlify-cli dependency and configuring vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import netlify from "@netlify/vite-plugin";
 // https://vite.dev/config/
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), netlify()],
     server: {
         port: 3000,
         open: true,
